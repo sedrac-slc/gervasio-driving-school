225940053c5ea83c696307bd465c1cb6489a28d8
sedrac-slc <slcsedrac@gmail.com>
2025-06-06 19:20:04 +0100

    config assets

---
+++ app/Models/Secretary.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Secretary extends Model
+{
+    //
+    use HasUuids;
+
+    protected $fillable = [
+        'user_id'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+}
